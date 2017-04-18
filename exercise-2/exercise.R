25698e8295be96b4ce7a2aa514c8bf83d9bfc509
Junjie Wu <junjiewu@D-69-91-176-136.dhcp4.washington.edu>
2017-04-18 13:57:38 -0700

    Made changes

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -16,21 +16,22 @@ devtools::install_github("hadley/fueleconomy")
 
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997 <- filter(vehicles, year >= 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique()
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+eco <- filter(vehicles, drive == '2-Wheel Drive', cty> 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+worst.id <- eco$id[eco$hwy == min(eco$hwy)]
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
-
+most.hwy <- function(year, make)
+  
+  
 
 # What was the most efficient honda model of 1995?
-
-
+eco.model <- filter(vehicles, make == 'Honda', year == 1995) & select(vehicles, )
